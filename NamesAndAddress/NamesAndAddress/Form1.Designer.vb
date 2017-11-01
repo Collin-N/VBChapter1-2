aeb233ee3581067f03212073439cb8bf911e49a9
Collin N <bubba9n500@gmail.com>
2017-11-01 10:35:09 -0500

    update new Names and address

---
+++ NamesAndAddress/NamesAndAddress/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.btnShowInfo = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo.Location = New System.Drawing.Point(59, 9)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(161, 95)
+        Me.lblInfo.TabIndex = 0
+        Me.lblInfo.Text = "William Wolfe" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "888 South Main Street" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Waynesville,NC 28786"
+        Me.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblInfo.Visible = False
+        '
+        'btnShowInfo
+        '
+        Me.btnShowInfo.Location = New System.Drawing.Point(30, 132)
+        Me.btnShowInfo.Name = "btnShowInfo"
+        Me.btnShowInfo.Size = New System.Drawing.Size(75, 23)
+        Me.btnShowInfo.TabIndex = 1
+        Me.btnShowInfo.Text = "Show Info"
+        Me.btnShowInfo.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(178, 132)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 209)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnShowInfo)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Name = "Form1"
+        Me.Text = "Name and Address"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnShowInfo As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblInfo As System.Windows.Forms.Label
+
+End Class
